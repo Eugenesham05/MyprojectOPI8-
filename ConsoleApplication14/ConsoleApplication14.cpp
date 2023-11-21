db744551af2d96491c0446dd38f9228f130e545a
Eugene <olia.shamray82@gmail.com>
2023-11-21 14:46:15 +0200

    ut

---
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+using namespace std;
+
+void ini(int a[])
+{
+	for (int i = 0; i < 10; i++)
+		a[i] = rand() % (101) - 70;
+} 
+void mass_t(int a[], int t[])
+{
+	for (int i = 0; i < 10; i++)
+		if (a[i] > 0) t[i] = a[i];
+		else t[i] = i;
+}
+
+int main()
+{
+	srand(time(NULL));
+	const int m = 10;
+	int n[m], b[m];
+	ini(n);
+	mass_t(n, b);
+	for (int i = 0; i < m; i++)
+		cout << n[i] << '\t' << b[i] << endl;
+	system("pause");
+	return 0;
+}
+
